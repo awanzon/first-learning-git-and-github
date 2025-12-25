7f097ad3860379182e89acbbb35ceb939881bff8
Sidiq Hikmawan <awanhikmawan02@gmail.com>
2025-12-25 22:24:33 +0700

    Update JS at Falsey Values

--- JavaScript Crash Course/index.js
+++ JavaScript Crash Course/index.js
@@ -1,3 +1,4 @@
+/*
 let string = "JavaScript Crash Course A";
 console.log(string[string.length - 1]); // Menghitung panjang string dan mengurangi 1 untuk mendapatkan indeks terakhir
 
@@ -32,6 +33,51 @@ let USD = 1200;
 let AUD = USD * 1.49;
 console.log(AUD);
 
-// Comparison Operators
+// Comparison Operators & Logical Operator
+
+Comparison Operator
+==
+===
+!=
+!==
+
+Logical Operator
+&& (and) operator
+|| (or) operator 
+
 let comparison = "5" == 5;
 console.log(comparison);
+
+
+// Conditionals
+
+let signIn = false;
+let hasMembership = false;
+
+if (signIn === true & hasMembership === true) {
+  console.log(`show user the page & Premium Content`);
+}
+else if (signIn === true & hasMembership !== true) {
+  console.log('Upgrade your Account To See the Premium Content');
+}
+else {
+  console.log(`Please log In first`);
+}
+
+
+
+let age = 17;
+
+if (age >= 18 && age <= 50){
+  console.log(`You're ${age} years old, you may enter the club. Welcome...`);
+}
+else if (age <= 18){
+  console.log(`You're ${age} years old, Get outta here!!!`);
+}
+else {
+  console.log(`You're ${age} years old, sorry but you're too old for this Environment. Enjoy your retirement`);
+}
+age = 18;
+*/
+
+// Falsey Values
