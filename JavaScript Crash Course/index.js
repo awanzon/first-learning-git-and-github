12fde06c7854b878493a028a4363d7a121224f11
Sidiq Hikmawan <awanhikmawan02@gmail.com>
2025-12-25 14:15:30 +0700

    Update JS Course at Comparison Operator

--- JavaScript Crash Course/index.js
+++ JavaScript Crash Course/index.js
@@ -1,8 +1,37 @@
 let string = "JavaScript Crash Course A";
 console.log(string[string.length - 1]); // Menghitung panjang string dan mengurangi 1 untuk mendapatkan indeks terakhir
 
+// let Keyword
 let name = "Awan";
 let namee = "KSAwan";
-var x = "I'm" + " " + 23 + " years old";
-const sex = "Male";
+let go = "I'm" + " " + 23 + " years old";
+let sex = "Male";
 console.log(sex);
+let result = 12 % 3;
+let age = 24;
+let a = "hello";
+(function(){
+  console.log("hi")
+})();
+
+let x = 3;
+let y = 4;
+console.log(x + y);
+
+
+
+console.log(result);
+console.log(age);
+
+// const keyword
+const size = 30%2;
+console.log(size);
+
+//exercise
+let USD = 1200;
+let AUD = USD * 1.49;
+console.log(AUD);
+
+// Comparison Operators
+let comparison = "5" == 5;
+console.log(comparison);
