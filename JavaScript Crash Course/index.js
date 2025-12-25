7fa5cdb79c8c4e81be44ec4715ee176b3cd958fb
Sidiq Hikmawan <awanhikmawan02@gmail.com>
2025-12-25 11:11:16 +0700

    Update Learn JS

---
+++ JavaScript Crash Course/index.js
@@ -0,0 +1,8 @@
+let string = "JavaScript Crash Course A";
+console.log(string[string.length - 1]); // Menghitung panjang string dan mengurangi 1 untuk mendapatkan indeks terakhir
+
+let name = "Awan";
+let namee = "KSAwan";
+var x = "I'm" + " " + 23 + " years old";
+const sex = "Male";
+console.log(sex);
